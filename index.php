1921ac58782d3239c0c9cbcb0f66778aad447d5a
Art-mj1 <mjmjpc@naver.com>
2021-08-11 10:10:45 +0900

    08/10 반경 km 내 캠핑 조회 가능

--- index.php
+++ index.php
@@ -32,10 +32,25 @@
  <!-- Search and Select Bar -->
  <div class="contents_bar">
   <div class="center">
-   <div class="search_bar">
-    <input type="text" placeholder="검색할 키워드를 입력해주세요">
-    <button><img src="/lbcamp/img/gocamp_icon.png" alt=""></button>
-   </div>
+      <div class="search_bar">
+         <input type="text" placeholder="검색할 키워드를 입력해주세요">
+         <button><img src="/lbcamp/img/gocamp_icon.png" alt=""></button>
+      </div>
+      <div class="drop_bar">
+         <div class="dropdown">
+            <div class="select">
+               <span>내 주변 캠핑장 검색</span>
+               <i class="fa fa-chevron-down"></i>
+            </div>
+            <ul class="dropdown_menu">
+              <li><a href="/lbcamp/search_position.php?radi=10000">반경 10KM 검색</a></li>
+              <li><a href="/lbcamp/search_position.php?radi=20000">반경 20KM 검색</a></li>
+              <li><a href="/lbcamp/search_position.php?radi=30000">반경 30KM 검색</a></li>
+              <li><a href="/lbcamp/search_position.php?radi=40000">반경 40KM 검색</a></li>
+              <li><a href="/lbcamp/search_position.php?radi=50000">반경 50KM 검색</a></li>
+            </ul>
+         </div>
+      </div>
   </div>
  </div>
 </div>
